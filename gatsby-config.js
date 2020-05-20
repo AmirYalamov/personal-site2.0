81f7c90811d7bb098887139d04c3e3b1026a082b
Amir Yalamov <amir.yalamov@gmail.com>
2020-05-20 17:01:53 -0400

    delete path config

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,6 @@ module.exports = {
     description: "The personal website of Amir Yalamov who studies at University of Western Ontario, and the site contains his projects, experience, and info.",
     author: "Amir Yalamov"
   },
-  pathPrefix: "/src/pages",
   plugins: [
     `gatsby-plugin-sharp`,
     {
